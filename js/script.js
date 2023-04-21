9c327df71ccb7c6f9bd294abcf6c868cebd8a5ef
durujibright <duruji.b.ifeanyi@gmail.com>
2023-04-21 13:51:36 +0100

    responsive news slider

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// var counter = 0;
+
+// slider()
+
+// function slider () {
+//     var imgslider = document.getElementsByClassName('heroimg');
+//     for (var i = 0; i < imgslider.length; i++) {
+//         imgslider[i].style.display = 'none'
+//     }
+
+//     counter++;
+
+//     if (counter > imgslider.length) {
+//         counter = 1;
+//     }
+
+//     imgslider[counter -1].style.display = 'block';
+//     setTimeout(slider, 4000)
+// }
+
+
+// OWL-CAROUSEL SLIDER
+
+let postSection = document.getElementsByClassName('post-section')
+
